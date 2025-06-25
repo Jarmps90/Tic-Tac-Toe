626e5f36517f9b1e9a9f245284f90356d04af899
Jarmps90 <160318973+Jarmps90@users.noreply.github.com>
2025-06-25 08:23:29 +0300

    Fixed typo

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ function Gameboard() {
   const colums = 3;
   const board = [];
 
-  for(let i = 0; i < 3; i++) {
+  for(let i = 0; i < rows; i++) {
     board[i] = [];
-    for(let j = 0; j < 3; j++) {
+    for(let j = 0; j < colums; j++) {
       board[i].push(0)
     };
   };
