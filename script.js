33345015864c8553ff7175a68e4192ac1ad1e965
Jarmps90 <160318973+Jarmps90@users.noreply.github.com>
2025-06-26 08:08:54 +0300

    Added code

--- script.js
+++ script.js
@@ -1,19 +1,83 @@
-function Gameboard() {
+const Gameboard = (function() {
   const rows = 3;
   const colums = 3;
-  const board = [];
+  const gameBoard = [];
 
   for(let i = 0; i < rows; i++) {
-    board[i] = [];
+    gameBoard[i] = [];
     for(let j = 0; j < colums; j++) {
-      board[i].push(0)
+      gameBoard[i].push(0)
     };
   };
   
-  const getBoard = () => board;
+  const getBoard = () => gameBoard;
 
+   //Displays printed borard 
+    const printBoard = () => {
+        for(let row of gameBoard) {
+            const rowString = row.join(' ')
+            console.log(rowString)
+        }
+    };
+  return {getBoard, printBoard}
+})();
+
+
+
+function gameControll() {
+    const board = Gameboard.getBoard()
+    
+//Module that defines palyers 
   
-  return {getBoard}
+    const players = {
+        playerOne : 'X',
+        playerTwo : 'O'
+        };
+    const {playerOne, playerTwo} = players;
+
+    let activePlayer = playerOne;
+
+//Function that switches between player
+    const switchPlayers = () => {
+        if(activePlayer === playerOne) {
+            activePlayer = playerTwo;
+        
+        } else if(activePlayer === playerTwo) {
+            activePlayer = playerOne;
+    }};
+
+//Module that adds tokkens
+    const addToken = (row, colum) => {
+        const tokenX = 'X';
+        const tokenO = 'O';
+        
+        
+        if((board[row][colum] === tokenO) || (board[row][colum] === tokenX)) {
+            return console.log('This is invalid move')
+        } else {
+            board[row][colum] = activePlayer;
+            return switchPlayers();
+        }
+        
+    };
+    
+function winningContitions() {
+    if((board[0][0] === 'X') && (board[0][1] === 'X') && (board[0][2] === 'X') || (board[0][0] === 'O') && (board[0][1] === 'O') && (board[0][2] === 'O') ||
+        (board[1][0] === 'X') && (board[1][1] === 'X') && (board[1][2] === 'X') || (board[1][0] === 'O') && (board[1][1] === 'O') && (board[1][2] === 'O')){
+        console.log('You are winner')
+    }
+};
+    
+    return{addToken, switchPlayers, winningContitions}
 };
 
+const gameOn = gameControll();
 
+gameOn.addToken(1,0);
+gameOn.addToken(0,1);
+gameOn.addToken(1,2);
+gameOn.addToken(0,0);
+gameOn.addToken(2,0);
+gameOn.addToken(0,2)
+gameOn.winningContitions();
+Gameboard.printBoard()
