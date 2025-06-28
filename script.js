a17caa103f772bdc2202f937c1d647dc7bd31aca
Jarmps90 <160318973+Jarmps90@users.noreply.github.com>
2025-06-28 22:02:50 +0300

    Uncomment conditions for testing new ones

--- script.js
+++ script.js
@@ -19,7 +19,8 @@ const Gameboard = (function() {
             console.log(rowString)
         }
     };
-  return {getBoard, printBoard}
+   
+    return {getBoard, printBoard}
 })();
 
 
@@ -58,13 +59,16 @@ function gameControll() {
         
     };
     
-function winningContitions() {
-    if((board[0][0] === 'X') && (board[0][1] === 'X') && (board[0][2] === 'X') || (board[0][0] === 'O') && (board[0][1] === 'O') && (board[0][2] === 'O') ||
-        (board[1][0] === 'X') && (board[1][1] === 'X') && (board[1][2] === 'X') || (board[1][0] === 'O') && (board[1][1] === 'O') && (board[1][2] === 'O')){
-        console.log('You are winner')
-    }
-};
+function winningContitions(board) {
+    // if((board[0][0] === 'X') && (board[0][1] === 'X') && (board[0][2] === 'X') || (board[0][0] === 'O') && (board[0][1] === 'O') && (board[0][2] === 'O') ||
+    //     (board[1][0] === 'X') && (board[1][1] === 'X') && (board[1][2] === 'X') || (board[1][0] === 'O') && (board[1][1] === 'O') && (board[1][2] === 'O')){
+    //     console.log('You are winner')
+    // }
     
+};
+
+
+  
     return{addToken, switchPlayers, winningContitions}
 };
 
@@ -78,4 +82,4 @@ gameOn.addToken(2,0);
 gameOn.addToken(0,2);
 
 gameOn.winningContitions();
-Gameboard.printBoard()
+// Gameboard.printBoard()
