cd1cf947e6c30c050a69353edaa20eba4f28748e
Jarmps90 <160318973+Jarmps90@users.noreply.github.com>
2025-07-04 07:35:45 +0300

    Instructions

--- script.js
+++ script.js
@@ -61,7 +61,7 @@ function gameControll() {
     };
     
 function winningContitions() {
-   
+   //Rethink youre winning contitions
     if((board[0][0] === 'X') && (board[0][1] === 'X') && (board[0][2] === 'X') || (board[0][0] === 'O') && (board[0][1] === 'O') && (board[0][2] === 'O') ||
         (board[1][0] === 'X') && (board[1][1] === 'X') && (board[1][2] === 'X') || (board[1][0] === 'O') && (board[1][1] === 'O') && (board[1][2] === 'O')){
         console.log('You are winner')
