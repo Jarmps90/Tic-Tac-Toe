92cb62ebb5ea3a8e2f85bde5ecb9cb6c7e64a7f4
Jarmps90 <160318973+Jarmps90@users.noreply.github.com>
2025-08-14 09:26:24 +0300

    Finalized winning conditions. Removed old comments. Added diagonals conditions

--- script.js
+++ script.js
@@ -57,10 +57,10 @@ function gameControll() {
             board[row][colum] = activePlayer;
             return switchPlayers();
       }
-    };67 
+    };
     
 function winningContitions() {
-   //Rethink you're winning contitions
+   
    //Try to add some empty check before winning contidions checks
    
     if(!board.length) return;
@@ -81,23 +81,53 @@ function winningContitions() {
 
         return{firstRow, secondRow, thirdRow};
     };
+    
+    function columns() {
+        const firstColumn = [];
+        const secondColumn = [];
+        const thirdColumn = [];
+
+        for(let i = 0; i < board.length; i++) {
+            firstColumn[i] = board[i][0];
+            secondColumn[i] = board[i][1];
+            thirdColumn[i] = board[i][2];
+        };
+        return{firstColumn, secondColumn, thirdColumn};
+    };
+
+    function diagonals() {
+        const diagonal1 = [];
+        const diagonal2 = [];
+
+        for(let i = 0, k = 1, l = 2; i < board.length; i++) {
+            diagonal1[i] = board[i][0];
+            diagonal1[k] = board[k][1];
+            diagonal1[l] = board[k][2];
+        };
+        for(let a = 0, b = 1, c = 2; a < board.length; a++) {
+            diagonal2[0] = board[0][a];
+            diagonal2[1] = board[b][1];
+            diagonal2[2] = board[c][0];
+        };
+        return{diagonal1, diagonal2};
+    };
 
     const row = rows();
+    const colum = columns();
+    const diagonal = diagonals();
 
     if((row.firstRow.every(isX) === true) || (row.firstRow.every(isO) === true) ||
         (row.secondRow.every(isX) === true) || (row.secondRow.every(isO) === true) ||
-        (row.thirdRow.every(isX) === true) || (row.thirdRow.every(isO) === true)) {
+        (row.thirdRow.every(isX) === true) || (row.thirdRow.every(isO) === true) ||
+        (colum.firstColumn.every(isX) === true) || (colum.firstColumn.every(isO) === true) ||
+        (colum.secondColumn.every(isX) === true) || (colum.secondColumn.every(isO) === true) ||
+        (colum.thirdColumn.every(isX) === true) || (colum.thirdColumn.every(isO) === true) ||
+        (diagonal.diagonal1.every(isX) === true) || (diagonal.diagonal1.every(isO) === true) ||
+        (diagonal.diagonal2.every(isX) === true) || (diagonal.diagonal2.every(isO) === true)) {
         console.log('You are winner')
     };
     
-    // if( (board[0].every(isX) === true) || (board[0].every(isO) === true) || 
-    //     (board[1].every(isX) === true) || (board[1].every(isO) === true) ||
-    //     (board[2].every(isX) === true) || (board[2].every(isO) === true)) {
-    //     console.log('You are winner')
-    // };
-
-   
-};
+    };
     return{addToken, switchPlayers, winningContitions}
 };
 
